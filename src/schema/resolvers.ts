625d7e11c4786dc569586d1e16fd73383ba78386
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-09-13 13:40:43 +0000

    individudal favMovie filter 🎬

--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -5,7 +5,12 @@ const resolver = {
         users: () => db.userList,
         user: (parent: any, args: { id: number }) => db.userList.find(user => Number(user.id) === Number(args.id)),
         movies: () => db.movies,
-        movie: (parent: any, args: { title: string }) => db.movies.find(movie => movie.title === args.title)
+        movie: (parent: any, args: { title: string }) => db.movies.find(movie => movie.title === args.title),
+    },
+    User: {
+        favoriteMovies: () => {
+            return db.movies.filter(movie => movie.yearOfPublication > 2000 && movie.yearOfPublication < 2010)
+        }
     }
 }
 
